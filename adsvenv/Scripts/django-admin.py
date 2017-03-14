e2461b0c9f0e68f46b2b6877634ec209abc91ad5
Denis Petrenko <denis.petrenko.dp@gmail.com>
2017-03-14 19:25:49 +0300

    init: venv, django

---
+++ adsvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\dpetrenko\desktop\denis\_work_dir\dev\adstat\adsvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
