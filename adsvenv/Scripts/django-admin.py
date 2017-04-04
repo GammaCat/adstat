e0bec6ed80130e3a40994b01b22e754271ae9a9e
Denis Petrenko <denis.petrenko.dp@gmail.com>
2017-04-04 17:05:29 +0300

    del venv from rep

--- adsvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\dpetrenko\desktop\denis\_work_dir\dev\adstat\adsvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
